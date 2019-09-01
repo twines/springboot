9d15629d2b1e290a1c43c622d9c8b07a9f7f8c23
1355081829@qq.com <1355081829aA>
2019-09-01 09:50:12 +0800

    springboot集成redis和springsession做session服务器

---
+++ src/main/java/com/hanyun/lee/config/WebSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.hanyun.lee.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+    @Bean
+    public HandlerInterceptor getSessionInterceptor() {
+        return new RedisSessionInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //所有已api开头的访问都要进入RedisSessionInterceptor拦截器进行登录验证，并排除login接口(全路径)。必须写成链式，分别设置的话会创建多个拦截器。
+        //必须写成getSessionInterceptor()，否则SessionInterceptor中的@Autowired会无效
+        registry.addInterceptor(getSessionInterceptor()).addPathPatterns("/api/**").excludePathPatterns("/api/user/login");
+    }
+}
+
+//拦截登录失效的请求
+class RedisSessionInterceptor implements HandlerInterceptor {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //无论访问的地址是不是正确的，都进行登录验证，登录成功后的访问再进行分发，404的访问自然会进入到错误控制器中
+        HttpSession session = request.getSession();
+        if (session.getAttribute("loginUserId") != null) {
+            try {
+                //验证当前请求的session是否是已登录的session
+                String loginSessionId = redisTemplate.opsForValue().get("loginUser:" + (long) session.getAttribute("loginUserId"));
+                if (loginSessionId != null && loginSessionId.equals(session.getId())) {
+                    return true;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        response401(response);
+        return false;
+    }
+
+    private void response401(HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+//        try
+//        {
+//            response.getWriter().print(JSON.toJSONString(new ReturnData(StatusCode.NEED_LOGIN, "", "用户未登录！")));
+//        }
+//        catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
