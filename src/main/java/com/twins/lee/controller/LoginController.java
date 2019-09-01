b0f8fb7f1b4f0b3ea46654b6d8c01f311ed880c8
1355081829@qq.com <1355081829aA>
2019-09-01 23:27:26 +0800

    mybatis-plush 分页单元测试 https://mybatis.plus 教程

--- src/main/java/com/twins/lee/controller/LoginController.java
+++ src/main/java/com/twins/lee/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.twins.lee.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.twins.lee.entity.User;
 import com.twins.lee.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,9 +31,17 @@ public class LoginController {
         System.out.print(iUserService.getUserById(1));
         return "login";
     }
+
     @RequestMapping("/doLogin")
     public String login(HttpServletRequest request, String account, String password) {
         User user = iUserService.getUserById(1);
         return "";
+    }
+
+    @RequestMapping("/userList")
+    @ResponseBody
+    public IPage<User> useList(@RequestParam(value = "page", required = false, defaultValue = "1") long p) {
+        Page page = new Page<User>(p, 2);
+        return iUserService.selectUserByPage(page, 1);
     }
 }
