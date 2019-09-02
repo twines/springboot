03eae350479d975d317138aa93896108c8d302eb
sk <lylapp@163.com>
2019-09-02 10:41:33 +0800

    1 修复无法运行 2 正则匹配

--- card_ocr/src/main/java/com/xsk/ocr/OcrStart.java
+++ card_ocr/src/main/java/com/xsk/ocr/OcrStart.java
@@ -13,13 +13,63 @@ import java.util.regex.Pattern;
 
 public class OcrStart {
     public static void main(String[] args) {
+
+        final String[] commonCode = {""};
+        final String[] ownerCode = {""};
+        final String[] idCard = {null};
+
+
+                ocr("card.jpeg", new OcrResult() {
+                    @Override
+                    public void onSucces(String result) {
+                        Pattern pattern = Pattern.compile("社会信用代码+\\d+p");
+                        Matcher matcher = pattern.matcher(result);
+                        if (matcher.find()) {
+                            commonCode[0] = matcher.group();
+                        }
+                        matcher = Pattern.compile("D法定代表人[^\\x00-\\xff]{2,4},C").matcher(result);
+                        if (matcher.find()) {
+                            ownerCode[0] = matcher.group();
+                        }
+                        System.out.println(commonCode);
+                        System.out.println(ownerCode);
+                    }
+
+                    @Override
+                    public void onFailed(Exception e) {
+
+                    }
+                });
+
+
+
+                ocr("idcard.jpg", new OcrResult() {
+                    @Override
+                    public void onSucces(String result) {
+                        Pattern pattern = Pattern.compile("\\d{6}(\\d{4})(\\d{2})(\\d{2})(\\d{3})([0-9]|X)");
+                        Matcher matcher = pattern.matcher(result);
+                        if (matcher.find()) {
+                            idCard[0] = matcher.group();
+                        }
+                    }
+
+                    @Override
+                    public void onFailed(Exception e) {
+
+                    }
+                });
+
+        System.out.println(commonCode[0]+"\t"+ownerCode[0]+"\t"+idCard[0]);
+    }
+
+    private static void ocr(String imgName, OcrResult ocrResult) {
         String sysPath = null;
         try {
             sysPath = ResourceUtils.getFile("classpath:").getAbsolutePath();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        String imgPath = sysPath+"/Img.jpeg";
+        String imgPath = sysPath + "/" + imgName;
         String tranPath = sysPath;
         System.out.println(imgPath);
         File imageFile = new File(imgPath);
@@ -27,13 +77,25 @@ public class OcrStart {
         instance.setDatapath(tranPath);
         instance.setLanguage("chi_sim");
 //        instance.setLanguage("eng");
-
+        String commonCode = "";
+        String ownerCode = "";
         try {
-            String result = instance.doOCR(imageFile);
-            System.out.println(result);
+            String result = instance.doOCR(imageFile).replace(" ", "");
+            if (ocrResult != null) {
+                ocrResult.onSucces(result);
+            }
 
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
+            if (ocrResult != null) {
+                ocrResult.onFailed(e);
+            }
         }
+    }
+
+    protected interface OcrResult {
+        void onSucces(String result);
+
+        void onFailed(Exception e);
     }
 }
