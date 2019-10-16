c908d48860bc67fedf8e5ed46673a83cb77aea0c
sk <lylapp@163.com>
2019-10-16 12:02:26 +0800

    数据库表修复

--- web/src/main/resources/db/ledan.sql
+++ web/src/main/resources/db/ledan.sql
@@ -5,13 +5,13 @@
  Source Server Type    : MySQL
  Source Server Version : 80012
  Source Host           : localhost:3306
- Source Schema         : spring
+ Source Schema         : ledan
 
  Target Server Type    : MySQL
  Target Server Version : 80012
  File Encoding         : 65001
 
- Date: 05/09/2019 13:34:54
+ Date: 16/10/2019 12:00:46
 */
 
 SET NAMES utf8mb4;
@@ -27,15 +27,9 @@ CREATE TABLE `improv` (
   `name` varchar(255) NOT NULL COMMENT '法人姓名',
   `code_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '企业营业执照',
   `name_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '法人身份证',
+  `state` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of improv
--- ----------------------------
-BEGIN;
-INSERT INTO `improv` VALUES (758, '12345678', '323', '/tmp/8b83122d-0fb2-4325-9ac5-6602da53e1f1.png', '/tmp/0fe9f07d-90fa-4029-839b-2fcc3c3fcc39.jpg');
-COMMIT;
 
 -- ----------------------------
 -- Table structure for permission
@@ -54,15 +48,6 @@ CREATE TABLE `permission` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of permission
--- ----------------------------
-BEGIN;
-INSERT INTO `permission` VALUES (1, '用户管理', 'menu', '\'userInfo/userList', 'userInfo:view', 0, '0/', 1);
-INSERT INTO `permission` VALUES (2, '用户添加', 'button', 'userInfo/userAdd', 'userInfo:add', 1, '0/1', 1);
-INSERT INTO `permission` VALUES (3, '用户删除', 'button', 'userInfo/userDel', 'userInfo:del', 1, '0/1', 1);
-COMMIT;
-
--- ----------------------------
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
@@ -75,14 +60,6 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of role
--- ----------------------------
-BEGIN;
-INSERT INTO `role` VALUES (1, 'admin', '系统管理员', 1);
-INSERT INTO `role` VALUES (2, 'vip', 'VIP会员\'', 1);
-COMMIT;
-
--- ----------------------------
 -- Table structure for role_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `role_permission`;
@@ -92,14 +69,6 @@ CREATE TABLE `role_permission` (
   `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of role_permission
--- ----------------------------
-BEGIN;
-INSERT INTO `role_permission` VALUES ('1', 1, 1);
-INSERT INTO `role_permission` VALUES ('2', 1, 2);
-COMMIT;
 
 -- ----------------------------
 -- Table structure for user
@@ -114,14 +83,6 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of user
--- ----------------------------
-BEGIN;
-INSERT INTO `user` VALUES (1, 'lee', 'e10adc3949ba59abbe56e057f20f883e', 'Lee');
-INSERT INTO `user` VALUES (2, 'jack', 'e10adc3949ba59abbe56e057f20f883e', 'Jack');
-COMMIT;
-
--- ----------------------------
 -- Table structure for user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
@@ -130,14 +91,5 @@ CREATE TABLE `user_role` (
   `user_id` int(11) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user_role
--- ----------------------------
-BEGIN;
-INSERT INTO `user_role` VALUES (1, 1, 1);
-INSERT INTO `user_role` VALUES (2, 2, 2);
-INSERT INTO `user_role` VALUES (3, 2, 1);
-COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
