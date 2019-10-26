cb33c7d77faf0b2683e959fdb2f8ea1d641295ff
sk <li.yl@gener-soft.com>
2019-10-26 21:56:40 +0800

    提取出OCR 和 二维码识别的功能

--- web/src/main/java/com/twins/lee/utilites/Utility.java
+++ web/src/main/java/com/twins/lee/utilites/Utility.java
@@ -1,5 +1,14 @@
 package com.twins.lee.utilites;
 
+import cn.hutool.extra.qrcode.BufferedImageLuminanceSource;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.common.HybridBinarizer;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.shiro.SecurityUtils;
@@ -12,12 +21,15 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
+import static org.apache.catalina.manager.Constants.CHARSET;
+
 public class Utility {
 
-    public static Map decodeLeDanInfo(Map<String,Object> userInfo) throws UnsupportedEncodingException {
+    public static Map decodeLeDanInfo(Map<String, Object> userInfo) throws UnsupportedEncodingException {
         for (String key : userInfo.keySet()) {
             String userInfoValue = (String) userInfo.get(key);
             userInfoValue = URLDecoder.decode(userInfoValue, "utf-8");
@@ -30,6 +42,7 @@ public class Utility {
         String strId = (String) userInfo().get("id");
         return Long.parseLong(strId);
     }
+
     public static Map userInfo() {
         Subject subject = SecurityUtils.getSubject();
         // 第一个放的是id
@@ -41,26 +54,27 @@ public class Utility {
 
         return (Map) infos.get(1);
     }
-    public static List<String> pdfToImagePath(String filePath){
+
+    public static List<String> pdfToImagePath(String filePath) {
         List<String> list = new ArrayList<>();
-        String fileDirectory = filePath.substring(0,filePath.lastIndexOf("."));//获取去除后缀的文件路径
+        String fileDirectory = filePath.substring(0, filePath.lastIndexOf("."));//获取去除后缀的文件路径
 
         String imagePath;
         File file = new File(filePath);
         try {
             File f = new File(fileDirectory);
-            if(!f.exists()){
+            if (!f.exists()) {
                 f.mkdir();
             }
             PDDocument doc = PDDocument.load(file);
             PDFRenderer renderer = new PDFRenderer(doc);
             int pageCount = doc.getNumberOfPages();
-            for(int i=0; i<pageCount; i++){
+            for (int i = 0; i < pageCount; i++) {
 // 方式1,第二个参数是设置缩放比(即像素)
                 BufferedImage image = renderer.renderImageWithDPI(i, 296);
 // 方式2,第二个参数是设置缩放比(即像素)
 //                BufferedImage image = renderer.renderImage(i, 5f); //第二个参数越大生成图片分辨率越高，转换时间也就越长
-                imagePath = fileDirectory + "/"+i + ".jpg";
+                imagePath = fileDirectory + "/" + i + ".jpg";
                 ImageIO.write(image, "PNG", new File(imagePath));
                 list.add(imagePath);
             }
@@ -69,4 +83,57 @@ public class Utility {
         }
         return list;
     }
+
+    /**
+     * OCR 通过文件绝对地址和OCR库在Resources资源中的绝对地址识别出问题
+     *
+     * @param destPath 图片本地地址
+     * @param tranPath OCR训练资源库地址
+     * @return 返回OCR识别的文本
+     */
+    public String ocrReco(String destPath, String tranPath) {
+
+        File imageFile = new File(destPath);
+        ITesseract instance = new Tesseract();
+        instance.setDatapath(tranPath);
+        instance.setLanguage("chi_sim");
+//        instance.setLanguage("eng");
+
+        try {
+            String result = instance.doOCR(imageFile).replace(" ", "");
+
+            return result;
+        } catch (TesseractException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 根据图片文件的绝对地址识别出二维码数据
+     *
+     * @param imageFilePath
+     * @return
+     * @throws IOException
+     * @throws NotFoundException
+     */
+    public String qrReco(String imageFilePath) throws IOException, NotFoundException {
+        String destPath = imageFilePath;
+
+        BufferedImage image;
+        image = ImageIO.read(new File(destPath));
+        if (image == null) {
+            return null;
+        }
+        BufferedImageLuminanceSource source = new BufferedImageLuminanceSource(image);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        com.google.zxing.Result result;
+        Hashtable<DecodeHintType, Object> hints = new Hashtable<DecodeHintType, Object>();
+        hints.put(DecodeHintType.CHARACTER_SET, CHARSET);
+        result = new MultiFormatReader().decode(bitmap, hints);
+
+        String resultStr = result.getText();
+
+        return resultStr;
+    }
+
 }
