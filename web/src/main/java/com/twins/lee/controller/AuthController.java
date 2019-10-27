9d4ec7292cc6f766e37635ea2cccf56d7e5eb50a
1355081829@qq.com <1355081829aA>
2019-10-27 18:55:33 +0800

    单据填写 上传

--- web/src/main/java/com/twins/lee/controller/AuthController.java
+++ web/src/main/java/com/twins/lee/controller/AuthController.java
@@ -2,7 +2,12 @@ package com.twins.lee.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class AuthController {
@@ -10,5 +15,13 @@ public class AuthController {
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("auth/index");
         return modelAndView;
+    }
+
+    @PostMapping("/auth/doAuth")
+    @ResponseBody
+    public Map<String, Object> doAuth() {
+        Map<String, Object> map = new HashMap<>();
+
+        return map;
     }
 }
